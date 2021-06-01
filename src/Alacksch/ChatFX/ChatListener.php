05e88e6a5355c70aa398e21d230f948bc3da2a71
Atlqs <ale3x90@gmail.com>
2021-06-01 19:37:53 +0400

    Cleaning code

--- src/Alacksch/ChatFX/ChatListener.php
+++ src/Alacksch/ChatFX/ChatListener.php
@@ -19,8 +19,6 @@ class ChatListener implements Listener {
     {
         if (isset($this->getPlugin()->CFXUsers[$event->getPlayer()->getName()])) {
             if ($this->getPlugin()->CFXUsers[$event->getPlayer()->getName()] === "Rainbow") {
-                $message = $this->getPlugin()->Rainbow($event->getMessage());
-                $event->setMessage($message);
                 $event->setMessage($this->getPlugin()->Rainbow($event->getMessage()));
             } else {
                 $color = $this->getPlugin()->CFXUsers[$event->getPlayer()->getName()];
