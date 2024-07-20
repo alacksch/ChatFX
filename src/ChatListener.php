66d31f0baf0b0a51161ec97e315660e7980eab2a
Haitham <52435658+alacksch@users.noreply.github.com>
2024-07-20 18:01:24 +0300

    Update ChatListener.php

--- src/ChatListener.php
+++ src/ChatListener.php
@@ -5,6 +5,7 @@ namespace Alacksch\ChatFX;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
 use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
 
 class ChatListener implements Listener {
     public function getOwningPlugin(): Plugin {
@@ -15,6 +16,9 @@ class ChatListener implements Listener {
             $fx = $this->getOwningPlugin()->CFXUsers[$event->getPlayer()->getName()] ?? null;
             if ($fx !== null) {
                 if ($fx->canUse($event->getPlayer())) $event->setMessage($fx->formatText($event->getPlayer(), $event->getMessage()));
+            } else {
+                unset($this->getOwningPlugin()->CFXUsers[$event->getPlayer()->getName()]);
+                $event->getPlayer()->sendMessage(TextFormat::RED . 'Your chat effects have been reset because your permissions changed');
             }
         }
     }
